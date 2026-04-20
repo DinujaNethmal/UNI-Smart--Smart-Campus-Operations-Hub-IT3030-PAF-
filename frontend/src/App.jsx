091028956ce4d66da23688b911f83b0e89445265
DinujaNethmal <dinujanethmal119@gmail.com>
2026-04-20 17:02:34 +0530

    feat: register Facility Catalogue routes in App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppShell from './components/AppShell';
+import RequireAdmin from './components/RequireAdmin';
+import DashboardPage from './pages/DashboardPage';
+import CreateBookingPage from './pages/CreateBookingPage';
+import MyBookingsPage from './pages/MyBookingsPage';
+import AdminBookingsPage from './pages/AdminBookingsPage';
+import FacilitiesPage from './pages/catalogue/FacilitiesPage';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppShell>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/new" element={<CreateBookingPage />} />
+          <Route path="/my-bookings" element={<MyBookingsPage />} />
+          <Route path="/admin" element={
+            <RequireAdmin>
+              <AdminBookingsPage />
+            </RequireAdmin>
+          } />
+          <Route path="/facilities" element={<FacilitiesPage />} />
+
+        </Routes>
+      </AppShell>
+    </BrowserRouter>
+  );
+}
+
+export default App;
